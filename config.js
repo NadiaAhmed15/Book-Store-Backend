31bcae3b9e52a549ee66835f15ee40baee5ab392
NadiaAhmed15 <nadiaahmedoo15@gmail.com>
2025-01-16 23:49:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = 5555 ;
 export const MONGODB_URL =
 'mongodb+srv://Nadia:Nadia123456789@books-store2.oeexs.mongodb.net/?retryWrites=true&w=majority&appName=Books-store2';
